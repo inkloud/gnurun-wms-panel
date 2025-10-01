ca2d03811b1f7437fd19818f749d4fb36fba9497
Luca Bacchi <bacchilu@gmail.com>
2025-10-01 10:37:59 +0200

    feat(backend): implement mock auth endpoint
      - add POST /auth that validates hard-coded users and returns a token
      - raise 401 for invalid credentials to mimic auth failure handling
      - depend on pyjwt to generate mock access tokens

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,37 @@
-from fastapi import FastAPI
+import jwt
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+USERS: dict[str, str] = {"bacchilu@gmail.com": "bacchilu", "luca@life365.eu": "luca"}
+SECRET: str = "Salve, mondo!"
+
+
+def check_authentication(username: str, password: str) -> dict | None:
+    pwd = USERS.get(username, None)
+    if pwd == password:
+        return {
+            "access_token": jwt.encode({"username": username}, SECRET),
+            "user": {"username": username},
+        }
 
 
 @app.get("/")
 async def root():
     return {"Hello": "World"}
+
+
+class AuthRequest(BaseModel):
+    username: str
+    password: str
+
+
+@app.post("/auth")
+async def authenticate(payload: AuthRequest):
+    if (data := check_authentication(payload.username, payload.password)) is not None:
+        return data
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials"
+    )
