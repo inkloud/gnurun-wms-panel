c3f8978f12eb21b9fe3d1dbac49245867269647c
Luca Bacchi <bacchilu@gmail.com>
2025-10-09 09:44:06 +0200

    chore: align tooling updates
      - Upgrade backend Docker base image to python 3.14.0
      - Register react-hooks as flat config plugin to restore linting
      - Bump frontend package versions to match eslint plugin update

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,19 +5,26 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
+const reactHooksRecommended = reactHooks.configs['recommended-latest']
+
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      ...tseslint.configs.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...reactHooksRecommended.rules,
     },
   },
 ])
